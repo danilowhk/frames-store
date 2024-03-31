80a9d631856f00a7c3e0308c3438f42c7849b32a
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MBP-2.fibertel.com.ar>
2024-03-31 18:32:26 -0300

    add tests

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -198,5 +198,41 @@ app.transaction("/buy/:price", async (c) => {
   });
 });
 
+app.frame('/test', (c) => {
+  return c.res({
+    action: '/test-finish',
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Perform a transaction
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Value (ETH)" />,
+      <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
+    ]
+  })
+})
+
+app.frame('/test-finish', (c) => {
+  const { transactionId } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Transaction ID: {transactionId}
+      </div>
+    )
+  })
+})
+app.transaction('/send-ether', (c) => {
+  const { inputText } = c
+  // Send transaction response.
+  return c.send({
+    chainId: 'eip155:10',
+    to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+    value: parseEther(inputText as string),
+  })
+})
+ 
+
 export const GET = handle(app);
 export const POST = handle(app);
